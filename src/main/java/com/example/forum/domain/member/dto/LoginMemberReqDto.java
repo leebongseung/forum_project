2f973f6c1f01f6876c5a4781970fb63c6c96efda
leebongseung <yui5227@gmail.com>
2024-09-10 02:14:42 +0900

    refactor : login @valid 축소 - 비밀번호 위험성 줄였음.

--- src/main/java/com/example/forum/domain/member/dto/LoginMemberReqDto.java
+++ src/main/java/com/example/forum/domain/member/dto/LoginMemberReqDto.java
@@ -14,12 +14,8 @@ import lombok.Setter;
 @AllArgsConstructor(staticName = "of")
 public class LoginMemberReqDto {
     @NotBlank(message = "아이디는 필수 입력 값입니다.")
-    @Size(min = 5, max = 20, message = "아이디는 5자 이상 20자 이하로 입력해주세요.")
-    @Pattern(regexp = "^[a-z0-9_-]+$", message = "아이디는 영문 소문자, 숫자, 하이픈(-), 언더스코어(_)만 사용 가능합니다.")
     private String loginId; // 로그인 아이디
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{10,}$",
-            message = "비밀번호는 대소문자, 숫자, 특수문자를 최소 1개 이상 포함하며, 10자 이상이어야 합니다.")
     private String password; // 로그인 비밀번호
 }
